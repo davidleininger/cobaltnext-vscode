6c1a3c884cc964a5458bbc67113724b9c2b371d6
David Leininger <dleininger@lifeteen.com>
2017-11-29 17:07:41 -0600

    Update React Demo to show text in JSX

--- demo/react.js
+++ demo/react.js
@@ -1,9 +1,10 @@
-import { Component } from 'React';
+import { Component } from "React";
 
 class Wow extends Component {
   render() {
     return (
       <div className="wow">
+        <p>Here is text</p>
         <input type="number" />
       </div>
     );
