ff77a3ebf9e84cfde4503836c95d1559ace37c18
David Leininger <davidbleininger@gmail.com>
2020-08-07 16:53:42 -0500

    MOAR

--- demo/php.php
+++ demo/php.php
@@ -1,8 +1,42 @@
 
 
 <?php
-  $age = 31;
+  $age = 33;
   $name = 'David';
   $cool = true;
 
-
+  class HelloWorldTest extends PHPUnit_Framework_TestCase
+  {
+      /**
+       * @var PDO
+       */
+      private $pdo;
+      public function setUp()
+      {
+          $this->pdo = new PDO($GLOBALS['db_dsn'], $GLOBALS['db_username'], $GLOBALS['db_password']);
+          $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+          $this->pdo->query("CREATE TABLE hello (what VARCHAR(50) NOT NULL)");
+      }
+      public function tearDown()
+      {
+          $this->pdo->query("DROP TABLE hello");
+      }
+      public function testHelloWorld()
+      {
+          $helloWorld = new HelloWorld($this->pdo);
+          $this->assertEquals('Hello World', $helloWorld->hello());
+      }
+      public function testHello()
+      {
+          $helloWorld = new HelloWorld($this->pdo);
+          $this->assertEquals('Hello Bar', $helloWorld->hello('Bar'));
+      }
+      public function testWhat()
+      {
+          $helloWorld = new HelloWorld($this->pdo);
+          $this->assertFalse($helloWorld->what());
+          $helloWorld->hello('Bar');
+          $this->assertEquals('Bar', $helloWorld->what());
+      }
+  }
+  ?>
